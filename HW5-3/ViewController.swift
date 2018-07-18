40f6352e6ece332c355b94443b705a0241d8d46c
mingsc <KevinChen.tw@gmail.com>
2018-07-19 07:00:13 +0800

    versin 1.0

--- HW5-3/ViewController.swift
+++ HW5-3/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    //播放按钮
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var playButton2: UIButton!
+    
+    
+    var player:AVPlayer?
+    func playUsingAVPlayer(url: URL) {
+        player = AVPlayer(url: url)
+        player?.play()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-
+    
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        
+        guard let url = URL(string: "https://s3.ap-south-1.amazonaws.com/aksharpatel47-static/positive_attitude.mp3") else {
+            print("Invalid URL")
+            return
+        }
+        playUsingAVPlayer(url: url)
+        
+    }
+    var audioPlayer: AVAudioPlayer?
+    
+    func playUsingAVAudioPlayer(url: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func playButton2Tapped(_ sender: Any) {
+        //return
+        
+        guard let filePath = Bundle.main.path(forResource: "StarSky", ofType: "mp3") else {
+            print("File does not exist in the bundle.")
+            return
+        }
+        
+        let url = URL(fileURLWithPath: filePath)
+        
+        playUsingAVAudioPlayer(url: url)
+    }
 }
 
